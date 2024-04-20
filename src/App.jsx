04926ef0f9203fb625e288ee4e5a68446a40f015
Franco <frangarcia93@icloud.com>
2024-04-20 12:28:28 -0300

    Se arma el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+
+import NavBarPublic from './components/navbar/NavBarPublic';
+import ItemListContainer from './components/home/ItemListContainer';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const name = "Franco";
+  const lastName = "Garcia";
+  const companyName = "Terminal Digital";
+  const author = "Realizado por " + name + ' ' + lastName;
+  const text = '¡Bienvenido al sistema de venta de pasajes de ' + companyName + '!';
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <NavBarPublic />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ItemListContainer text={text} author={author} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
