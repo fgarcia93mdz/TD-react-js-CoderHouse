82c71ff35272b83ef85563fece9df6819825f99c
Franco <frangarcia93@icloud.com>
2024-05-04 12:41:31 -0300

    Configuracion

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/TD-react-js-CoderHouse/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 })
