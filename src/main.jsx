8890d89600b282c91cbf763e098bcb04b8135044
Franco <frangarcia93@icloud.com>
2024-05-06 19:07:23 -0300

    Correcciones por fallas de package

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <BrowserRouter basename="/TD-react-js-CoderHouse/">
-      <App />
-    </BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 )
