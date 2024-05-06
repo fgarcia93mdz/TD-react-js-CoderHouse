50929f14f5f25b071b8289344f3056b51ca6069e
Franco <frangarcia93@icloud.com>
2024-05-06 19:09:58 -0300

    CardContext y CartWidget Comentados para la entrega final

--- src/components/card/CartWidget.jsx
+++ src/components/card/CartWidget.jsx
@@ -1,29 +1,29 @@
-import React, { useContext } from 'react';
-import Button from '@mui/material/Button';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { CartContext } from './CartContext';
+// import React, { useContext } from 'react';
+// import Button from '@mui/material/Button';
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import { CartContext } from './CartContext';
 
-const CartWidget = () => {
-  const { cart, clearCart } = useContext(CartContext);
+// const CartWidget = () => {
+//   const { cart, clearCart } = useContext(CartContext);
 
-  const itemCount = cart.reduce((total, item) => total + (Number.isFinite(item.quantity) ? item.quantity : 0), 0);
+//   const itemCount = cart.reduce((total, item) => total + (Number.isFinite(item.quantity) ? item.quantity : 0), 0);
   
-  return (
-    <div>
-      <Button onClick={clearCart} disableRipple>
-        <ShoppingCartIcon style={{
-          color: "#0E315A",
-          fontSize: "30px",
-          position: "relative",
-          top: "-5px",
-        }} />
-        <span style={{
-          position: "relative",
-          top: "-25px",
-        }}>{itemCount}</span>
-      </Button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Button onClick={clearCart} disableRipple>
+//         <ShoppingCartIcon style={{
+//           color: "#0E315A",
+//           fontSize: "30px",
+//           position: "relative",
+//           top: "-5px",
+//         }} />
+//         <span style={{
+//           position: "relative",
+//           top: "-25px",
+//         }}>{itemCount}</span>
+//       </Button>
+//     </div>
+//   );
+// };
 
-export default CartWidget;
+// export default CartWidget;
