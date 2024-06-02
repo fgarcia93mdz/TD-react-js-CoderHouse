1f09567a3dca0493b14c0f148820745c8578fa1a
Franco <frangarcia93@icloud.com>
2024-06-02 20:01:34 -0300

    Correccion estetica

--- src/components/order/OrderPage.jsx
+++ src/components/order/OrderPage.jsx
@@ -33,7 +33,7 @@ const OrderPage = () => {
         boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)',
         margin: '2%',
       }}>
-        <Typography variant="h5">Detalles de la Orden</Typography>
+        <Typography variant="h5" style={{ color: '#000000' }}>Detalles de la Orden</Typography>
       </Box>
       <Box boxShadow={3} p={2}>
         <TableContainer>
